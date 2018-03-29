af06bad27e9a85b99b87507be3ab68d340a191bc
Donghee Han <hdh7485@gmail.com>
2018-03-29 16:19:25 +0900

    Add u_turn_action.py

--- scripts/u_turn_action.py
+++ scripts/u_turn_action.py
@@ -12,20 +12,20 @@ from obstacle_detector.msg import SegmentObstacle
 from geometry_msgs.msg import Point
 
 from action_with_smach.msg import MissionPlannerAction, MissionPlannerGoal, MissionPlannerResult, MissionPlannerFeedback
-from dynamic_avoidance import DynamicAvoidance
+from u_turn import Uturn 
 
 def execute_cb(goal):
     rospy.loginfo("Goal Received")
-    dynamic_mission = DynamicAvoidance()
+    u_turn = Uturn()
     result = MissionPlannerResult()
     result.time_elapsed = rospy.Duration(1)
-    dynamic_mission.execute()
+    u_turn.execute()
     action_server.set_succeeded(result)
 		
 if __name__ == '__main__':
-    rospy.init_node('dynamic_avoidance', anonymous=True)
+    rospy.init_node('u_turn', anonymous=True)
     try:
-        action_name = 'dynamic_avoidance'
+        action_name = 'u_turn'
         action_server = actionlib.SimpleActionServer(action_name, MissionPlannerAction, execute_cb=execute_cb, auto_start=False)
         action_server.start()
         rospy.spin()
